05566d2e807c693bdc68311d21627ba8ce1cfc6e
BehindLoader <simonsuprun@gmail.com>
2018-06-27 00:28:01 +0300

    х у е т а

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,9 @@
+from os import path
+
 from flask import Flask
 
 server = Flask(__name__)
+server.template_folder = path.abspath('./templates/')
+server.static_folder = path.abspath('./static/')
+
 from server import views
--- server/views.py
+++ server/views.py
@@ -1,7 +1,29 @@
-from server import server
-from flask import render_template
+from server import server, settings
+from flask import render_template, send_file, jsonify
+from os import path
+import requests
 
-@server.route('/')
-@server.route('/index')
-def index():
-    return render_template('../templates/index.html')
+# @server.route('/')
+# @server.route('/index')
+@server.route('/<string:board>')
+def index(board = 'b'):
+    site = dict(
+        title = settings.SITE_HEADER,
+        board = board
+    )
+    return render_template(
+        'index.html',
+        site = site
+    )
+
+@server.route('/api/boards')
+def get_boards():
+    r = requests.get('http://2ch.hk/makaba/mobile.fcgi?task=get_boards')
+    return jsonify( r.json() )
+
+@server.route('/src/<path:filename>')
+def get_static(filename):
+    """
+    returns static file
+    """
+    return send_file( path.join(settings.STATIC_FOLDER, filename) )
